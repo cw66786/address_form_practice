18df93ff033f8b6ffd7493618ac7ff074327c131
Craig Wilson <cw66786@gmail.com>
2023-04-20 18:06:51 -0500

    all initial set up and components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AddressFormComponent } from './pages/address-form/address-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddressFormComponent
   ],
   imports: [
     BrowserModule,
